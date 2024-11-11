633abc7c7f1e85d084ec8054afb34fc8248d0838
Murat Akkus <akkusmurat123@gamil.com>
2024-11-11 01:44:05 +0100

    aa

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 
-export default defineConfig(() => {
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
   return {
     define: {
-      __APP_ENV__: process.env.VITE_BASE_URL,
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
     plugins: [
       vue(),
